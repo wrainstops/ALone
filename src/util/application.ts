4c5faf154fc9babe3b8319b54ebcb343877f8433
cuizenglong <czlong2520@163.com>
2025-11-12 15:05:09 +0800

    :fire:

--- src/util/application.ts
+++ src/util/application.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
-import { type ApplicationOptions } from '#/application'
+import { type ApplicationOptions } from '#/application';
 
 export default class Application {
   // canvas
