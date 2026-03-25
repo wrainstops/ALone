c63d54a71b671ce13ab6ffbed9bd01094f580d4e
cuizenglong <czlong2520@163.com>
2026-03-25 10:31:59 +0800

    :lipstick: 404 and homePage

--- apps/frontplay/src/views/error/404.vue
+++ apps/frontplay/src/views/error/404.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="w-full h-full flex items-center justify-center bg-[#dbdbdc]">
-    <n-image :src="Img404" preview-disabled object-fit="contain"></n-image>
+  <div class="w-full h-full relative bg-[#0c0c0cee]">
+    <LetterGlitch />
+    <div
+      class="flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-500/50 shadow-lg shadow-gray-500/70 p-16 rounded-full"
+    >
+      <div class="text-white text-8xl font-bold pointer-events-none">404</div>
+      <div class="text-white text-6xl font-bold pointer-events-none">You are lost</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NImage } from 'naive-ui'
-import Img404 from '@/assets/image/404.png'
+import LetterGlitch from '@/component/LetterGlitch/LetterGlitch.vue'
 </script>
--- apps/frontplay/src/views/homePage/index.vue
+++ apps/frontplay/src/views/homePage/index.vue
@@ -4,7 +4,7 @@
     <div
       class="flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
     >
-      <h1 class="text-white text-4xl font-bold pointer-events-none">Front Play</h1>
+      <n-h1 class="text-white! text-4xl font-bold pointer-events-none">Front Play</n-h1>
       <div class="flex gap-4 mt-4">
         <n-button type="primary" dashed @click="handleClick">开始使用</n-button>
       </div>
@@ -14,7 +14,7 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { NButton } from 'naive-ui'
+import { NH1, NButton } from 'naive-ui'
 import Galaxy from '@/component/Galaxy/Galaxy.vue'
 
 const router = useRouter()
